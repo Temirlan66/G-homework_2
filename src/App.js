c1d1b4c5ff833ef41b7cb8e2ebce866b07d27828
Temirlan <admin@Temirlans-MacBook-Pro.local>
2023-05-20 00:56:36 +0600

    homework-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class BluebLock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: "blue",
+    };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <button
+          className={this.state.color}
+          onClick={() => this.setState({ color: "red" })}
+        ></button>
+        <Redblock />
+      </div>
+    );
+  }
 }
 
-export default App;
+class Redblock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: "red",
+    };
+  }
+
+  render() {
+    return (
+      <button
+        className={this.state.color}
+        onClick={() => this.setState({ color: "blue" })}
+      ></button>
+    );
+  }
+}
+
+export default BluebLock;
